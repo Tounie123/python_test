e21a4ca964be72ae36ab166a839769300e3eed31
Tounie123 <740762622@qq.com>
2018-11-22 23:22:32 +0800

    12.5

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
 	#初始化游戏并创建一个屏幕对象
@@ -11,6 +13,7 @@ def run_game():
 	#screen = pygame.display.set_mode((1200, 800))
 	pygame.display.set_caption("Alien Invasion")
 
+	ship = Ship(screen)
 	#设置背景色
 	#bg_color = (230,230,230)
 
@@ -25,7 +28,10 @@ def run_game():
 		#每次循环时都重绘屏幕
 		#screen.fill(bg_color)
 		screen.fill(ai_settings.bg_color)
+		ship.blitme()
 
+		gf.check_events()
+		gf.update_screen(ai_settings, screen,ship)
 		# 让最近绘制的屏幕可见
 		pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -1,11 +1,8 @@
 class Settings():
 	"""存储《外星人入侵》的所有设置的类"""
 
-	def __init__(self, screen):
+	def __init__(self):
 		"""初始化游戏的设置"""
-		self.screen = screen
-
-		#加载飞船图像并获取其外接矩形
 		#屏幕设置
 		self.screen_width = 1200
 		self.screen_height = 800
